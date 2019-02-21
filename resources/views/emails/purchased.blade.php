026941cac5fe76ece7b0a20904830faceb2623fb
Vishmanthi <fernandovishmanthi@yahoo.com>
2019-02-21 07:21:06 +0530

    initializing project

---
+++ resources/views/emails/purchased.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Purchase successful</title>
+</head>
+<body>
+    <h1>Thank you for purchasing</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eos magni amet aspernatur vitae enim minus explicabo alias magnam quam ullam atque, molestias facilis, pariatur neque quia? Officiis, quod hic.</p>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/',[
+    'uses'=>'FrontEndController@index',
+    'as'=>'index'
+]);
+Route::get('/product/{id}',[
+    'uses'=>'FrontEndController@singleProduct',
+    'as'=>'product.single'
+]);
+Route::post('/cart/add',[
+    'uses'=>'shoppingcontroller@add_to_cart',
+    'as'=>'cart.add'
+]);
+Route::get('/cart',[
+    'uses'=>'shoppingcontroller@cart',
+    'as'=>'cart'
+]);
+Route::get('/cart/delete/{id}',[
+    'uses'=>'shoppingcontroller@cart_delete',
+    'as'=>'cart.delete'
+]);
+Route::get('/cart/incr/{id}/{qty}',[
+    'uses'=>'shoppingcontroller@incr',
+    'as'=>'cart.incr'
+]);
+
+Route::get('/cart/decr/{id}/{qty}',[
+    'uses'=>'shoppingcontroller@decr',
+    'as'=>'cart.decr'
+]);
+Route::get('/cart/rapid/add/{id}',[
+    'uses'=>'shoppingcontroller@rapid_add',
+    'as'=>'cart.rapid.add'
+]);
+Route::get('/cart/checkout',[
+    'uses'=>'CheckoutController@index',
+    'as'=>'cart.checkout'
+]);
+Route::post('/cart/checkout',[
+    'uses'=>'CheckoutController@pay',
+    'as'=>'cart.checkout'
+]);
+Route::resource('products','ProductsController');
+Auth::routes(); 
+Route::get('/home','HomeController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
